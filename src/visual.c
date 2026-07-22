d45ac1f1467edfd0f92dc75f4dda3704ade3aa10
Konstantinos Karatsioris <konstantinoskaratsioris@iMac-von-Konstantinos-6246.local>
2026-07-22 11:25:37 +0200

    visual: scale the represantation of the drawing

--- src/visual.c
+++ src/visual.c
@@ -1,5 +1,7 @@
 #include "calculator.h"
 
+#define REFERENCE_MAX_MM 6000.0
+
 static void	set_margin(GtkWidget *place, int num)
 {
 	gtk_widget_set_margin_start(place, num);
@@ -74,63 +76,72 @@ static void	print_dim_for_check(t_result *res)
 
 void draw_function (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer data)
 {
+	printf("WIDTH: %d", width);
+	printf("HEIGHT :%d", height);
 	t_result *res = (t_result *) data;
 	(void)res;
 	
+	float scale = width / REFERENCE_MAX_MM;
+
+
   	float width_frame = res->frame_width;
 	float height_frame = res->frame_height;
 
-	float width_sash = res->sash_width;
-	float height_sash = res->sash_height;
+	// float width_sash = res->sash_width;
+	// float height_sash = res->sash_height;
   	(void)area;
 
+	float scaled_frame_w = width_frame * scale;
+	float scaled_frame_h = height_frame * scale;
+
 	print_dim_for_check(res);
+
 	cairo_set_source_rgb (cr, 1.0, 1.0, 1.0); /* white */
 	cairo_paint (cr);
 
 	cairo_set_line_width (cr, 3.0);
 	cairo_set_source_rgb (cr, 0.0, 0.0, 0.0); /* black */
 	cairo_rectangle (cr,
-					width/2.0 - width_frame/2,
-					height/2.0 - height_frame/2,
-					width_frame,
-					height_frame);
+					width/2.0 - scaled_frame_w/2,
+					height/2.0 - scaled_frame_h/2,
+					scaled_frame_w,
+					scaled_frame_h);
 	cairo_stroke (cr);
 
-	cairo_set_line_width (cr, 1.5);
-	cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
-	cairo_rectangle (cr,
-					width/2.0 - width_frame/2 + 41,
-					height/2.0 - height_frame/2 + 41,
-					width_sash,
-					height_sash);
-	cairo_stroke (cr);
+	// cairo_set_line_width (cr, 1.5);
+	// cairo_set_source_rgb (cr, 0.0, 0.0, 0.0);
+	// cairo_rectangle (cr,
+	// 				width/2.0 - width_frame/2 + 41,
+	// 				height/2.0 - height_frame/2 + 41,
+	// 				width_sash,
+	// 				height_sash);
+	// cairo_stroke (cr);
 
-	int x1 = 50;
-	int y1 = 50;
-	int x2 = 200;
-	int y2 = 200;
-	// Κύρια γραμμή
-	cairo_move_to(cr, x1, y1);
-	cairo_line_to(cr, x2, y2);
-	cairo_stroke(cr);
+	// int x1 = 50;
+	// int y1 = 50;
+	// int x2 = 200;
+	// int y2 = 200;
+	// // Κύρια γραμμή
+	// cairo_move_to(cr, x1, y1);
+	// cairo_line_to(cr, x2, y2);
+	// cairo_stroke(cr);
 
-	// Μύτη βέλους στο (x2, y2) — δύο μικρές διαγώνιες γραμμές
-	double angle = atan2(y2 - y1, x2 - x1);  // κατεύθυνση της κύριας γραμμής
-	double arrow_len = 10.0;
-	double arrow_angle = 0.4; // ακτίνια, πόσο "ανοιχτή" είναι η μύτη
+	// // Μύτη βέλους στο (x2, y2) — δύο μικρές διαγώνιες γραμμές
+	// double angle = atan2(y2 - y1, x2 - x1);  // κατεύθυνση της κύριας γραμμής
+	// double arrow_len = 10.0;
+	// double arrow_angle = 0.4; // ακτίνια, πόσο "ανοιχτή" είναι η μύτη
 
-	cairo_move_to(cr, x2, y2);
-	cairo_line_to(cr,
-		x2 - arrow_len * cos(angle - arrow_angle),
-		y2 - arrow_len * sin(angle - arrow_angle));
-	cairo_stroke(cr);
+	// cairo_move_to(cr, x2, y2);
+	// cairo_line_to(cr,
+	// 	x2 - arrow_len * cos(angle - arrow_angle),
+	// 	y2 - arrow_len * sin(angle - arrow_angle));
+	// cairo_stroke(cr);
 
-	cairo_move_to(cr, x2, y2);
-	cairo_line_to(cr,
-		x2 - arrow_len * cos(angle + arrow_angle),
-		y2 - arrow_len * sin(angle + arrow_angle));
-	cairo_stroke(cr);
+	// cairo_move_to(cr, x2, y2);
+	// cairo_line_to(cr,
+	// 	x2 - arrow_len * cos(angle + arrow_angle),
+	// 	y2 - arrow_len * sin(angle + arrow_angle));
+	// cairo_stroke(cr);
 }
 
 void activate(GtkApplication *app, gpointer data)
@@ -139,26 +150,32 @@ void activate(GtkApplication *app, gpointer data)
 	t_window	*win = (t_window *) data;
 	t_result	*res = g_new0(t_result, 1);
 
+	
     GtkWidget	*window = gtk_application_window_new(app);
     GtkWidget	*area = gtk_drawing_area_new();
-    GtkWidget	*box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 10);
+    GtkWidget	*box = gtk_box_new(GTK_ORIENTATION_HORIZONTAL,0);
 	GtkWidget	*sidebar = build_sidebar(win, res, area);
-
-    calculate_dimensions(win, res);
-
+	
+	
+	calculate_dimensions(win, res);
+	
     gtk_widget_set_size_request(sidebar, 250, -1);
 	set_margin(sidebar, 40);
-
+	
 	gtk_widget_set_hexpand(area, TRUE);
     gtk_widget_set_vexpand(area, TRUE);
+	
     gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA(area), draw_function, res, NULL);
-
+	
     gtk_box_append(GTK_BOX(box), sidebar);
     gtk_box_append(GTK_BOX(box), area);
-
+	
     gtk_window_set_title(GTK_WINDOW(window), "Window calculator");
-    gtk_window_set_default_size(GTK_WINDOW(window), 1200, 1200);
+    // gtk_window_set_default_size(GTK_WINDOW(window), 1200, 1200);
     gtk_window_set_child(GTK_WINDOW(window), box);
+	
+	
+	gtk_window_maximize(GTK_WINDOW(window));
     gtk_window_present(GTK_WINDOW(window));
 
 }
